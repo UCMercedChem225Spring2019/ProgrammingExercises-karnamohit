6bffdf3fe623f251b59d5e01882a66268d2e9527
Karnamohit Ranka <kranka@ucmerced.edu>
2019-03-14 14:11:08 -0700

    solutions to pgrmEx02.pdf

--- pgrm_02_01.f90
+++ pgrm_02_01.f90
@@ -32,6 +32,10 @@
 ! WRITE CODE HERE TO READ THE ARRAY ELEMENTS FROM THE INPUT FILE.
 ! *************************************************************************
 !
+!      read(IIn,*)
+      do i=1,NDim**2
+        read(IIn,*)Array_Input(i)
+      enddo
 !
 !     Convert Array_Input to Matrix and print the matrix.
 !
@@ -64,6 +68,11 @@
 !     Loop through the elements of AMatOut and fill them appropriately from
 !     Array_Input.
 !
+      do i=1,N
+        do j=1,M
+          AMatOut(j,i) = ArrayIn((i-1)*N + j)
+        enddo
+      enddo
 !
 ! *************************************************************************
 ! WRITE CODE HERE TO READ THE ARRAY ELEMENTS FROM THE INPUT FILE.
@@ -91,6 +100,11 @@
 !     Loop through the elements of AMatOut and fill them appropriately from
 !     Array_Input.
 !
+      do i=1,N
+        do j=1,M
+          AMatOut(i,j) = ArrayIn((i-1)*N + j)
+        enddo
+      enddo
 !
 ! *************************************************************************
 ! WRITE CODE HERE TO READ THE ARRAY ELEMENTS FROM THE INPUT FILE.
@@ -99,3 +113,43 @@
 !
       Return
       End Subroutine Packed2Matrix_RowWise
+
+
+      Subroutine Print_Matrix_Full_Real(AMat,M,N)
+!
+!     This subroutine prints a real matrix that is fully dimension -
+!     i.e.,
+!     not stored in packed form. AMat is the matrix, which is
+!     dimensioned
+!     (M,N).
+!
+!     The output of this routine is sent to unit number 6 (set by the
+!     local
+!     parameter integer IOut).
+!
+!
+!     Variable Declarations
+!
+      implicit none
+      integer,intent(in)::M,N
+      real,dimension(M,N),intent(in)::AMat
+!
+!     Local variables
+      integer,parameter::IOut=6,NColumns=5
+      integer::i,j,IFirst,ILast
+!
+ 1000 Format(1x,A)
+ 2000 Format(5x,5(7x,I7))
+ 2010 Format(1x,I7,5F14.6)
+!
+      Do IFirst = 1,N,NColumns
+        ILast = Min(IFirst+NColumns-1,N)
+        write(IOut,2000) (i,i=IFirst,ILast)
+        Do i = 1,M
+          write(IOut,2010) i,(AMat(i,j),j=IFirst,ILast)
+        endDo
+      endDo
+!
+      Return
+      End Subroutine Print_Matrix_Full_Real
+
